6ae259ebbc7d3f1b4e071be06828fc4c4a7df7bd
Jack <JJB9922@protonmail.com>
2022-05-08 22:33:48 +0100

    Backtracking -- Learning

--- main.cpp
+++ main.cpp
@@ -1,83 +1,26 @@
 #include <iostream>
-#include <string.h>
+#include <string>
+#include <fstream>
+
+using namespace std;
 
 class records{
-    private:
-        char *artist, *album, *country, *label;
-        double *rating;
     public:
-        records() {
-            artist = new char[40];
-            album = new char[40];
-            country = new char[40];
-            label = new char [40];
-            rating = new double;
-        }
-        void addRecord();
-        void removeRecord();
-        void editRecord();
-        void showRecord();
+        string artist;
+        string album;
 
-        void addWishlist();
-        void removeWishlist();
-        void editWishlist();
-        void showWishlist();
-
-        int search(char[], char[]);
-
+    void addRecord(){
+        cout << "Input Artist: \n";
+        cin >> artist;
+        cout << "Input Album: \n";
+        cin >> album;
+    }    
 };
 
-void records::addRecord(){
-    std::cout << "\nEnter artist name:\n"; 
-    std::cin.getline(artist,40);
-    std::cout << "Enter album name:\n"; 
-    std::cin.getline(album,40);
-    std::cout << "Enter country of origin:\n"; 
-    std::cin.getline(country,40);
-    std::cout << "Enter label name:\n"; 
-    std::cin.getline(label,40);
-    std::cout << "Enter rating /5:\n"; 
-    std::cin >> *rating;
+int main ()
+{
     
-}
-
-void records::removeRecord(){
-
-}
-
-void records::editRecord(){
-    std::cout << "\nEnter artist name:\n"; 
-    std::cin.getline(artist,40);
-    std::cout << "Enter album name:\n"; 
-    std::cin.getline(album,40);
-    std::cout << "Enter country of origin:\n"; 
-    std::cin.getline(country,40);
-    std::cout << "Enter label name:\n"; 
-    std::cin.getline(label,40);
-    std::cout << "Enter rating /5:\n"; 
-    std::cin >> *rating;
-}
-
-void records::showRecord(){
-    std::cout << "Artist: " << artist; 
-    std::cout << "Album: " << album;
-    std::cout << "Country: " << country;
-    std::cout << "Label: " << label;
-    std::cout << "Rating: " << rating;
-}
-
-int main(){
-    records records;
-    records.addRecord();
-
-    /*struct{
-        std::string artist;
-        std::string album;
-        std::string country;
-        std::string label;
-        double rating;
-    } addRecStruct;*/
 
     
-    return 0;
+	return 0;
 }
