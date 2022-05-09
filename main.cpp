c9d010d3605f843501d64a53567ef90abba99887
Jack <JJB9922@protonmail.com>
2022-05-09 02:56:04 +0100

    Can now add records by saving them to file, can view all records in file

--- main.cpp
+++ main.cpp
@@ -17,10 +17,50 @@ class records{
     }    
 };
 
-int main ()
-{
-    
+int main(){
 
-    
-	return 0;
+    int menuOption;
+    int id;
+
+    records record;
+    fstream recordList("RecordList.txt", ios::in | ios::out | ios::app);
+
+    while(menuOption != 3){
+
+    cout << "\nMAIN MENU\n";
+    cout << "1. ADD RECORD\n";
+    cout << "2. VIEW ALL RECORDS\n";
+    cout << "3. EXIT\n";
+    cout << "\nSelection: ";
+    cin >> menuOption;
+    cout << endl;
+
+//Next task: incrementing ID
+        switch(menuOption){
+            case 1:   
+                record.addRecord();
+                         
+                if(recordList.is_open()){
+                    cout << "\nRecord added successfully.\n";
+                    recordList << "{" << id << "," << record.artist << "," << record.album << "}" << endl;
+            break;
+
+            case 2:
+                recordList.seekg(0);
+                    string line;
+                    while (recordList.good()){
+                        getline(recordList, line);
+                        cout << line << endl;
+                    }
+
+                    } else cout << "File is not there on the given path\n";
+            break;
+            default:
+            if(menuOption=3){
+                cout << "Exiting...";
+            }else cout << "\nPlease pick a valid option";
+            break;   
+        }
+    }
+
 }
